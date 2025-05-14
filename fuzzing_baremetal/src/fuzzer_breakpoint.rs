4aad9c5a8c35aceb26eb0296870c82ffecdbc549
matthewchavis8 <matthewchavis8@gmail.com>
2025-05-13 23:14:21 -0500

    feat: Created the run client to start the fuzzing process when a process is spawned. Created two observer channels Time Observer and a Map observer to track code coverage and the execution time. Then intialized the QEMU Emulator

--- fuzzing_baremetal/src/fuzzer_breakpoint.rs
+++ fuzzing_baremetal/src/fuzzer_breakpoint.rs
@@ -3,8 +3,10 @@
 
 use std::{env, path::PathBuf, time::Duration};
 
-use libafl_bolts::core_affinity::Cores;
-use libafl_qemu::{elf::EasyElf, GuestAddr};
+use libafl::{inputs::BytesInput, observers::{CanTrack, HitcountsMapObserver, TimeObserver, VarLenMapObserver, VariableMapObserver}};
+use libafl_bolts::{core_affinity::Cores, ownedref::OwnedMutSlice};
+use libafl_qemu::{elf::EasyElf, modules::StdEdgeCoverageModule, Emulator, GuestAddr};
+use libafl_targets::{edges_map_mut_ptr, EDGES_MAP_DEFAULT_SIZE, MAX_EDGES_FOUND};
 
 pub static mut MAX_INPUT_SIZE: usize = 50;
 
@@ -64,8 +66,51 @@ pub fn fuzz() {
         )
         .expect("env BREAKPOINT not set or having trouble finding BREAKPOINT in binary");
     println!("Break point address: {:#X}", breakpoint_addr);
+    
+    /*
+     * After broker is set up the qemu launcher will invoke to the client process once
+     * Basically each processes main function 
+     *
+     * @param state              - if restarting a worker it carries over else fresh start
+     * @param mgr                - event manager built with multimonitor to handle reporting with the broker
+     * @param client_description -
+     * */
+    let mut run_client = |state: Option<_>, mut mgr, _client_description| {
+        let args: Vec<String> = env::args().collect();
 
+        // Harness calling the LLVM-style harness
+        let mut harness = |
+            emulator: &mut Emulator<_,_,_,_,_,_,_,>,
+            state: &mut _,
+            input: &BytesInput| unsafe {
+            emulator.run(state, input).unwrap().try_into().unwrap()
+        };
+        
+        // Created an observeration channel to watch code coverage
+        let mut edges_observer = unsafe {
+            HitcountsMapObserver::new(VariableMapObserver::from_mut_slice(
+                    "edges", 
+                    OwnedMutSlice::from_raw_parts_mut(edges_map_mut_ptr(), EDGES_MAP_DEFAULT_SIZE), 
+                    &raw mut MAX_EDGES_FOUND,
+            ))
+            .track_indices()
+        };
+        
+        // Created an observation channel to keep track of execution time
+        let time_observer = TimeObserver::new("Time");
 
+        // Initialize QEMU Emulator
+        let emu = Emulator::builder()
+            .qemu_parameters(args)
+            .prepend_module(
+                StdEdgeCoverageModule::builder()
+                .build()
+                .expect("Failed to intialize coverage map in QEMU"),
+            )
+            .build()
+            .expect("Failed to call QEMU emulator");
+
+    };
 
     println!("Successfully BUILT");
 }
